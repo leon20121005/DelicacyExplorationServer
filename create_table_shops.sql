e5f3a1ff201d909ba79b4a14ae1f0fd11c274813
leon20121005 <leon20121005@gmail.com>
2017-08-30 15:24:34 +0800

    add attribute, latitude and longitude, in Shops

--- create_table_shops.sql
+++ create_table_shops.sql
@@ -1,9 +1,11 @@
 CREATE TABLE Shops
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     evaluation DECIMAL(3,1),
     address VARCHAR(20) NOT NULL,
+    latitude FLOAT(10, 6) NOT NULL,
+    longitude FLOAT(10, 6) NOT NULL,
     created_at timestamp default now(),
     updated_at timestamp
 );
--- populate_shops.sql
+++ populate_shops.sql
@@ -1,20 +1,20 @@
-INSERT INTO shops (name, evaluation, address)
-VALUES ("勝博殿", 10, "台北市信義區松山路11號2樓");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("薄多義", 10, "台北市信義區忠孝東路五段17號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("框框美式餐館", 10, "新北市新店區北新路三段78號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("hot 7 新鐵板料理", 10, "台北市文山區景中街1號2樓");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("CaliBurger", 10, "台北市萬華區漢中街107號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("陶板屋", 10, "台北市中正區重慶南路一段58號2樓");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("石二鍋", 10, "新北市新店區民權路15號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("瓦崎", 10, "台北市大安區敦化南路一段160巷60號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("山丼", 10, "台北市大安區羅斯福路三段25號");
-INSERT INTO shops (name, evaluation, address)
-VALUES ("饗食天堂", 10, "台北市信義區松壽路12號6樓");
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("勝博殿", 10, "台北市信義區松山路11號2樓", 25.049190, 121.578299);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("薄多義", 10, "台北市信義區忠孝東路五段17號", 25.041356, 121.565571);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("框框美式餐館", 10, "新北市新店區北新路三段78號", 24.980484, 121.542094);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("hot 7 新鐵板料理", 10, "台北市文山區景中街1號2樓", 24.993170, 121.541415);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("CaliBurger", 10, "台北市萬華區漢中街107號", 25.043259, 121.507455);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("陶板屋", 10, "台北市中正區重慶南路一段58號2樓", 25.043901, 121.513106);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("石二鍋", 10, "新北市新店區民權路15號", 24.982713, 121.542200);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("瓦崎", 10, "台北市大安區敦化南路一段160巷60號", 25.043513, 121.546526);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("山丼", 10, "台北市大安區羅斯福路三段25號", 25.023772, 121.525166);
+INSERT INTO shops (name, evaluation, address, latitude, longitude)
+VALUES ("饗食天堂", 10, "台北市信義區松壽路12號6樓", 25.035310, 121.566067);
