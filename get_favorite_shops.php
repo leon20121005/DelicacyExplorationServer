69cef8861c568d1ce7f80f8dff60776eed0c5e6a
leon20121005 <leon20121005@gmail.com>
2017-09-05 10:20:24 +0800

    create get_favorite_shops.php

---
+++ get_favorite_shops.php
@@ -0,0 +1,83 @@
+<?php
+    // Array for JSON response
+    $response = array();
+
+    // Include database connection class
+    require_once __DIR__ . '/db_connect.php';
+
+    // Connecting to db
+    $database = new DBConnection();
+
+    // Get parameters from post data
+    $id_list = json_decode($_POST["id_list"]);
+    $latitude = $_POST["latitude"];
+    $longitude = $_POST["longitude"];
+
+    // Create query condition WHERE
+    $is_first_element = true;
+    $id_list_string = "";
+    foreach ($id_list as $id)
+    {
+        if ($is_first_element)
+        {
+            $id_list_string .= $id;
+            $is_first_element = false;
+        }
+        else
+        {
+            $id_list_string .= (", " . $id);
+        }
+    }
+
+    // Get conditional shops from shops table. 6371 = earth radius (km)
+    $query = "SELECT *,".
+            " (6371 * acos(cos(radians($latitude)) * cos(radians(latitude)) * cos(radians(longitude) - radians($longitude)) + sin(radians($latitude)) * sin(radians(latitude))))".
+            " AS distance".
+            " FROM shops".
+            " WHERE id IN ($id_list_string)".
+            " ORDER BY distance";
+    $result = mysql_query($query);
+    if (!$result)
+    {
+        die("Invalid query: " . mysql_error());
+    }
+
+    // Check for empty result
+    if (mysql_num_rows($result) > 0)
+    {
+        // Looping through all results
+        // Shops node
+        $response["shops"] = array();
+
+        while ($row = mysql_fetch_array($result))
+        {
+            // Temp for shop array
+            $shop = array();
+            $shop["id"] = $row["id"];
+            $shop["name"] = $row["name"];
+            $shop["evaluation"] = $row["evaluation"];
+            $shop["address"] = $row["address"];
+            $shop["created_at"] = $row["created_at"];
+            $shop["updated_at"] = $row["updated_at"];
+
+            // Push single shop into final response array
+            array_push($response["shops"], $shop);
+        }
+
+        // Success
+        $response["success"] = 1;
+
+        // Echoing JSON response
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    }
+    else
+    {
+        // No shops found
+        $response["success"] = 0;
+        $response["message"] = "No shops found";
+
+        // Echo no shops JSON
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    }
+?>
+<?php
