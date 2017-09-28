b74e0984a1afef3974058fa37d536fd6f21f49a3
leon20121005 <leon20121005@gmail.com>
2017-09-28 15:18:35 +0800

    create comments table, add new data in shops table

--- create_table_comments.sql
+++ create_table_comments.sql
@@ -1,8 +1,10 @@
-CREATE TABLE Images
+CREATE TABLE Comments
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    image BLOB NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    url VARCHAR(20) NOT NULL,
+    address VARCHAR(20) NOT NULL,
+    evaluation DECIMAL(3,1),
     created_at timestamp default now(),
     updated_at timestamp,
     shop_id INT NOT NULL,
